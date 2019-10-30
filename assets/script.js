512261e391e79ae33dd03e5a540f5cf69af1c3f9
Marissa Fortier <marissafortier@gmail.com>
2019-10-29 21:33:59 -0400

    set the apis to variables in js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 var myKey = "6db8a71f3737a50245cbb91d2a90d2e1";
-var currentWeather = "api.openweathermap.org/data/2.5/weather?q={city name}";
-var fiveWeather = ""
+var currentWeather = `api.openweathermap.org/data/2.5/weather?q=${currentWeather}{city, name}`;
+var fiveWeather = `api.openweathermap.org/data/2.5/forecast?q=${fiveWeather}{city name},{country code}`;
+var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${uvIndex}&lat={lat}&lon={lon}`;
+
+
+
 
 
 
