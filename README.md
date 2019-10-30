ad63aad4e35c0514f0161d53566282634cc0edbb
Marissa Fortier <marissafortier@gmail.com>
2019-10-29 21:17:28 -0400

    fixed styling and started js file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Weather
+
+Create a weather app for mulitple cities so someone travelling can check the hourly foreceast for areas they are in.
---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var myKey = "6db8a71f3737a50245cbb91d2a90d2e1";
+var currentWeather = "api.openweathermap.org/data/2.5/weather?q={city name}";
+var fiveWeather = ""
+
+
+
+// $.ajax{(
+//     url: 
+// )}
