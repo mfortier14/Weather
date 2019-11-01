121340c93801c6bd159c85637b66b9fd41607e18
Marissa Fortier <marissafortier@gmail.com>
2019-11-01 19:54:27 -0400

    added ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
-var myKey = "6db8a71f3737a50245cbb91d2a90d2e1";
-var currentWeather = `api.openweathermap.org/data/2.5/weather?q=${currentWeather}{city, name}`;
-var fiveWeather = `api.openweathermap.org/data/2.5/forecast?q=${fiveWeather}{city name},{country code}`;
+var myKey = "&APPID=6db8a71f3737a50245cbb91d2a90d2e1";
+var currentCity
+var queryUrl = "api.openweathermap.org/data/2.5/weather?q=";
+var fiveWeather = `api.openweathermap.org/data/2.5/forecast?q=`;
 var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${uvIndex}&lat={lat}&lon={lon}`;
 
+// api.openweathermap.org/data/2.5/forecast?q=London&APPID=6db8a71f3737a50245cbb91d2a90d2e1
 
-
+function getForcast(city) {
+    $.ajax({
+        url: queryUrl + city + myKey,
+        method: "GET"
+    })
+    .then( function (response) {
+        console.log(response);
+    })
+}
 
 
 
