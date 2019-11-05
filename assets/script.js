e16d3222247b88de17f695cdc2e22a393a7b03ed
Marissa Fortier <marissafortier@gmail.com>
2019-11-04 20:02:36 -0500

    worked with tutor to create functions to be able to type in a city and have it save into local storage and then onto the page

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,76 @@
 var myKey = "&APPID=6db8a71f3737a50245cbb91d2a90d2e1";
-var currentCity
-var queryUrl = "api.openweathermap.org/data/2.5/weather?q=";
+var searchCities = [];
+var queryUrl = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=";
 var fiveWeather = `api.openweathermap.org/data/2.5/forecast?q=`;
-var uvIndex = `http://api.openweathermap.org/data/2.5/uvi?appid=${uvIndex}&lat={lat}&lon={lon}`;
 
-// api.openweathermap.org/data/2.5/forecast?q=London&APPID=6db8a71f3737a50245cbb91d2a90d2e1
+// ===========================================================================================================
+showSearchCities();
+$(document).on("click", "a", searchCitiesForecast);
+$(document).on("click", "button", buttonForecast);
 
-function getForcast(city) {
+// ===========================================================================================================
+
+function showSearchCities() {
+    if (localStorage.getItem("cities")){
+        searchCities = [ ...localStorage.getItem("cities").split(";")];
+        for (let i = 0; i < searchCities.length; i++) {
+            var $a = `<a href="#" class="list-group-item list-group-item-action">${searchCities[i]}</a>`
+            $(".list-group").prepend($a); 
+        };
+    };
+};
+
+function searchCitiesForecast() {
+    getForecast($(this).text());
+};
+
+function buttonForecast() {
+    var city = $("#inputPassword2").val();
+    $("#inputPassword2").val("");
+    searchCities.push(city);
+    $(".list-group").empty();
+    localStorage.setItem("cities", searchCities.join(";"));
+    showSearchCities();
+    getForecast(city);
+};
+
+// ===========================================================================================================
+
+function getForecast(city) {
+
     $.ajax({
         url: queryUrl + city + myKey,
         method: "GET"
     })
-    .then( function (response) {
-        console.log(response);
-    })
-}
+        .then(function (response) {
+            console.log(response);
+            
+            const { city, list } = response;
+            let $div = `<div class="ml-4">
+                <h1 class="display-3 text-center">${city.name}, ${city.country}</h1>
+                <hr>
+                <h2>${moment(list[0].dt_txt).format('llll')}</h2>
+                <h2>Temperature: ${parseInt((list[0].main.temp - 273.15) * 9 / 5 + 32)} °F</h2>
+                <h2>Humidity: ${list[0].main.humidity}</h2>
+                <h2>Description: ${list[0].weather[0].description}</h2>
+                <img class="mainIcon" src="http://openweathermap.org/img/w/${list[0].weather[0].icon}.png">
+            </div>`
+            $(".jumbotron").html($div);
 
+            $(".card-deck").empty();
+            for (let i = 3; i < list.length; i = i + 8) {
+                const { dt_txt, main, weather } = list[i];
+                const $card = `<div class="card text-white">
+                    <div class="card-body p-0">
+                        <p class="card-text day">${moment(dt_txt).format('dddd')}</p>
+                        <p class="card-text">Temp: ${parseInt((main.temp - 273.15) * 9 / 5 + 32)} °F</p>
+                        <p class="card-text">Hum:${main.humidity}</p>
+                        <p class="card-text">${weather[0].description}</p>
+                        <img class="m-auto" src="http://openweathermap.org/img/w/${weather[0].icon}.png">
+                    </div>
+                </div>`
 
-
-// $.ajax{(
-//     url: 
-// )}
+                $(".card-deck").append($card);
+            }
+        })
+};
